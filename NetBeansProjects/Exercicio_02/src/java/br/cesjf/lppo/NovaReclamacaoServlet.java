5b79079d52db9a4467b9caeb9db1ffc5d882be31
Adriano <Adriano@DESKTOP-UV6FNPB>
2017-03-19 14:06:32 -0300

    Criando metodo de inserção em banco.

---
+++ NetBeansProjects/Exercicio_02/src/java/br/cesjf/lppo/NovaReclamacaoServlet.java
@@ -0,0 +1,67 @@
+package br.cesjf.lppo;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author aluno
+ */
+@WebServlet(name = "NovaReclamacaoServlet", urlPatterns = {"/novo.html"})
+public class NovaReclamacaoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/novaReclamacao.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String nome = request.getParameter("nome");
+        String email = request.getParameter("email");
+        String descricao = request.getParameter("descricao");
+
+        Logger.getLogger(NovaReclamacaoServlet.class.getName()).log(Level.INFO, "POST: " + nome + "" + email + "" + descricao);
+
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            String url = "jdbc:derby://localhost:1527/lppo-2017-1";
+            Connection conexao = DriverManager.getConnection(url, "usuario", "senha");
+            System.out.println("Conexao aberta com sucesso!");
+
+            String sql = "INSERT INTO contato(nome, email, descricao, status) VALUES('"
+                    + nome + "', '"
+                    + email + "', '"
+                    + descricao + "', '"
+                    + 0 + "')";
+
+            Statement operacao = conexao.createStatement();
+            operacao.executeUpdate(sql);
+            System.out.println("Registro inserido!");
+
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Driver indisponivel!");
+            Logger.getLogger(NovaReclamacaoServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            System.err.println("Problema ao acessar o banco!");
+            Logger.getLogger(NovaReclamacaoServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.sendRedirect("lista.html");
+    }
+
+}
